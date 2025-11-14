af1d38d48cdd1304a3279e518614862d9e3162d8
keerthanak <keerthanak@ICS-LT-B8JS9K3>
2025-11-14 17:40:24 +0530

    Initial commit

---
+++ Online_Utility_Bill_System/Online_Utility_Bill_System/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Online_Utility_Bill_System
+{
+    internal class Customer
+    {
+        public int CustomerID;
+        public string CustomerName;
+
+       
+        public static double GetServiceCharge()
+        {
+            return 50.0; 
+        }
+
+       
+        public double TotalUsage(params int[] readings)
+        {
+            double total = 0;
+            foreach (int unit in readings)
+            {
+                total += unit * 6.5; 
+            }
+            return total;
+        }
+
+       
+        public void CalculateBill(out double total, out double tax, out double netPayable, params int[] readings)
+        {
+            total = TotalUsage(readings);
+            tax = total * 0.10; 
+            netPayable = GetServiceCharge() + total + tax;
+        }
+    }
+}
---
+++ Online_Utility_Bill_System/Online_Utility_Bill_System/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Online_Utility_Bill_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------Online Utility Billing System ------");
+
+            Console.Write("Enter number of customers: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nEnter details for Customer #{i}");
+                Customer customer = new Customer();
+
+                Console.Write("Customer ID: ");
+                customer.CustomerID = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Customer Name: ");
+                customer.CustomerName = Console.ReadLine();
+
+                Console.Write("Enter monthly usage readings (in units): ");
+                string[] input = Console.ReadLine().Split(' ');
+                int[] readings = new int[input.Length];
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    readings[j] = int.Parse(input[j]);
+                }
+
+                double total, tax, netPayable;
+                customer.CalculateBill(out total, out tax, out netPayable, readings);
+
+                Console.WriteLine("\n------ Utility Bill ------");
+                Console.WriteLine($"Customer ID   : {customer.CustomerID}");
+                Console.WriteLine($"Customer Name : {customer.CustomerName}");
+                Console.WriteLine($"Service Charge: {Customer.GetServiceCharge()}");
+                Console.WriteLine($"Total Usage   : {total}");
+                Console.WriteLine($"Tax Applied   : {tax}");
+                Console.WriteLine($"Net Payable   : {netPayable}");
+                Console.WriteLine("------------------------------");
+            }
+        }
+    }
+}
+
