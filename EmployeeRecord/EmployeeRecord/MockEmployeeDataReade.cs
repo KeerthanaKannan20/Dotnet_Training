ff68bc16afbdfcc784448e6b9232c4ab3cbc2570
keerthanak <keerthanak@ICS-LT-B8JS9K3>
2025-11-25 17:15:26 +0530

    Initial commit

---
+++ EmployeeRecord/EmployeeRecord/MockEmployeeDataReade.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRecord
+{
+    // Interface
+    public interface IEmployeeDataReader
+    {
+        Employeerecord GetEmployeeRecords(int employeeid);
+    }
+    // Concrete class
+    public class MockEmployeeDataReader : IEmployeeDataReader
+    {
+        public Employeerecord GetEmployeeRecords(int employeeid)
+        {
+            if (employeeid == 102)
+            {
+                return new Employeerecord
+                {
+                    Id = 102,
+                    Name = "Keerthana",
+                    Role = "Manager",
+                    IsVeteran = true
+                };
+            }
+            if (employeeid == 101)
+            {
+                return new Employeerecord
+                {
+                    Id = 101,
+                    Name = "Sanjay",
+                    Role = "Developer",
+                    IsVeteran = false
+                };
+            }
+            if (employeeid == 103)
+            {
+                return new Employeerecord
+                {
+                    Id = 103,
+                    Name = "Karthi",
+                    Role = "Intern",
+                    IsVeteran = false
+                };
+            }
+            return new Employeerecord
+            {
+                Id = employeeid,
+                Name = "Unknown",
+                Role = "Unknown",
+                IsVeteran = false
+            };
+        }
+    }
+}
---
+++ EmployeeRecord/EmployeeRecord/PayrollProcessor.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeRecord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class PayrollProcessor
+{
+    //Dictionary initializer
+    private static readonly Dictionary<int, decimal> BaseSalaries = new()
+    {
+        [101] = 65000m,
+        [102] = 120000m,
+        [103] = 30000m
+    };
+    private readonly IEmployeeDataReader _employeeReader;
+
+    // Constructor Injection
+    public PayrollProcessor(IEmployeeDataReader employeeReader)
+    {
+        _employeeReader = employeeReader;
+    }
+
+    // Pattern matching
+    public decimal CalculateTotalCompensation(int employeeId)
+    {
+        
+        var emp = _employeeReader.GetEmployeeRecords(employeeId);
+        BaseSalaries.TryGetValue(employeeId, out decimal baseSalary);
+        decimal bonus = emp switch
+        {
+            { Role: "Manager", IsVeteran: true } => 10000m,
+            { Role: "Manager", IsVeteran: false } => 5000m,
+            { Role: "Developer" } => 2000m,
+            { Role: "Intern" } => 500m,
+            _ => 0m 
+        };
+        return baseSalary + bonus;
+    }
+}
